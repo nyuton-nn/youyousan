2e10bf5ab6fb6db540e307b39d7118bbda0e272f
nyuton-nn <85528428+nyuton-nn@users.noreply.github.com>
2021-06-08 08:22:14 +0900

    test
    test

---
+++ App.js
@@ -0,0 +1,251 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { lighten, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+const useStyles = makeStyles({
+  table: {
+    width: "600px",
+  },
+  TableRow: {
+    backgroundColor: '#CCFFCC',
+    hover: "#11FFDD"
+  },
+  task: {
+    width: "300px"
+  },
+  status: {
+    width: "50px"
+  },
+  container: {
+    maxHeight: "400px",
+  },
+  box: {
+    display: "flex",
+    padding: "50",
+  },
+  box2: {
+    width: "650px",
+    padding: "100",
+  },
+  box3: {
+    align: "center",
+    width: "100px",
+    padding: "30",
+  },
+});
+
+let titles = new Map();
+titles.set("key1", "No");
+titles.set("key2", "task")
+
+let rawData = ['ABC', '20210525', '1', 'あいうえお', 0, '20210527', , '＠＠＠', '', '20210625']
+
+let leftData11 = []
+let leftData12 = []
+let leftData13 = []
+let leftData14 = []
+let leftData15 = []
+let leftData16 = []
+let leftData17 = []
+
+export default function TaskList() {
+  const classes = useStyles();
+  const [selected1, setSelected1] = React.useState(String);
+  const [selected2, setSelected2] = React.useState(String);
+  const [leftData, setleftData] = React.useState([
+    {'NO':1, 'len':[]},
+    {'NO':2, 'len':[]},
+    {'NO':3, 'len':[]},
+    {'NO':4, 'len':[]},
+    {'NO':5, 'len':[]},
+    {'NO':6, 'len':[]},
+    {'NO':7, 'len':[]},
+  ]);
+ const [rightData, setrightData] = React.useState([]);
+ const [printData, setprintData] = React.useState([]);
+ console.log(rightData)
+
+  const rightInit = () => {
+    let inputRight1 = []
+    for(let i=0;i<rawData.length;i++){
+      printData.push({'NO':i+1,'len':rawData[i]})
+      inputRight1.push({'NO':i+1})
+    }
+    setprintData(printData)
+    setrightData(inputRight1)
+    setleftData([
+      {'NO':1, 'len':[]},
+      {'NO':2, 'len':[]},
+      {'NO':3, 'len':[]},
+      {'NO':4, 'len':[]},
+      {'NO':5, 'len':[]},
+      {'NO':6, 'len':[]},
+      {'NO':7, 'len':[]},
+    ]);
+  }
+
+  const handleClick1 = (event, name) => {
+    let newSelected = [];
+    newSelected = newSelected.concat(name);
+    setSelected1(newSelected)
+  }
+  const handleClick2 = (event, name) => {
+    let newSelected = [];
+    newSelected = newSelected.concat(name);
+    setSelected2(newSelected)
+  }
+  const infolink = (event, aaa) => {
+    let newDatas1 = leftData
+    let newDatas2 = rightData
+    for (let i = 0; i < newDatas1.length; i++) {
+      if (newDatas1[i].NO == selected1) {
+        newDatas1[i].len.push(selected2)
+        break;
+      }
+    }
+    let delnum =[]
+    for (const elem1 of newDatas2) {
+      if (elem1.NO == selected2) {
+      }else{
+        delnum.push({'NO':elem1.NO})
+      }
+    }
+    setleftData(newDatas1);
+    setrightData(delnum)
+    let newSelected=parseInt(selected2)+1
+    handleClick2(event,newSelected)
+  }
+  const infolinkRe = (event, aaa) => {
+    let newDatas1 = leftData
+    let newDatas2 = rightData
+    let num1 = selected2
+    for (let i = 0; i < newDatas1.length; i++) {
+      if (newDatas1[i].NO == selected1) {
+        for(let j=newDatas1[i].len.length-1;0<=j;j--){
+          newDatas2.push({'NO':parseInt(newDatas1[i].len[j])})
+          newDatas1[i].len.pop()
+          break;
+        }
+        break;
+      }
+    }
+    newDatas2.sort(function(a, b) {
+      if (a.NO > b.NO) {
+        return 1;
+      } else {
+        return -1;
+      }
+    })
+    setleftData(newDatas1)
+    setrightData(newDatas2)
+    handleClick2(event,num1-1)
+  }
+
+  const printter1 = (name) => {
+    let stPrint =""
+    console.log("右表示処理" )
+    for (const elem1 of leftData) {
+      if (elem1.NO == name) {
+        for (let i =0;i<elem1.len.length;i++) {
+          for (const elem2 of printData) {
+            console.log(elem1.len[i] )
+            console.log(elem2.NO)
+            if (elem1.len[i] == elem2.NO) {
+              console.log(elem2.len)
+              stPrint = stPrint + elem2.len
+              console.log(stPrint)
+              break;
+            }
+          }
+        }
+        break;
+      }
+    }
+    return stPrint;
+  }
+  const printter2 = (name) => {
+    let stPrint
+    for (const elem1 of printData) {
+      if (elem1.NO == name) {
+        stPrint = elem1.len
+        break;
+      }
+    }
+    return stPrint;
+  }
+
+  return (
+    <div>
+      <div className={classes.box}>
+        <div className={classes.box2}>
+          <TableContainer className={classes.container}>
+            <Table stickyHeader aria-label="sticky table" className={classes.table}>
+              <TableHead>
+                <TableRow>
+                  <TableCell component="th" scope="row">{titles.get("key1")}</TableCell>
+                  <TableCell align="left" className={classes.task} >{titles.get("key2")}</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {leftData.map((row) => {
+                  return (
+                    <TableRow key={row.NO} onClick={(event) => handleClick1(event, row.NO)} className={selected1 == row.NO ? classes.TableRow : ""}>
+                      <TableCell component="th" scope="row">{row.NO}</TableCell>
+                      <TableCell align="left" className={classes.task}>{printter1(row.NO)}</TableCell>
+                    </TableRow>
+                  )
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+        <div className={classes.box3}>
+          <br />
+          <br />
+          <button onClick={(event) => rightInit(event, "init")}>
+            init
+          </button>
+          <br />
+          <br />
+          <button onClick={(event) => infolink(event, "紐づけ")}>
+            紐づけ
+          </button>
+          <br />
+          <br />
+          <button onClick={(event) => infolinkRe(event, "戻す")}>
+            戻す
+          </button>
+        </div>
+        <div className={classes.box2}>
+          <TableContainer className={classes.container}>
+            <Table stickyHeader aria-label="sticky table" className={classes.table}>
+              <TableHead>
+                <TableRow>
+                  <TableCell component="th" scope="row">{titles.get("key1")}</TableCell>
+                  <TableCell align="left" className={classes.task} >{titles.get("key2")}</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rightData.map((row) => {
+                  return (
+                    <TableRow key={row.NO} onClick={(event) => handleClick2(event,row.NO)} className={selected2 == row.NO ? classes.TableRow : ""}>
+                      <TableCell component="th" scope="row">{row.NO}</TableCell>
+                      <TableCell align="left" className={classes.task}>{printter2(row.NO)}</TableCell>
+                    </TableRow>
+                  )
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ App2.js
@@ -0,0 +1,312 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { lighten, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import { InputLabel } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  table: {
+    width: "600px",
+  },
+  TableRow: {
+    backgroundColor: '#CCFFCC',
+    hover: "#11FFDD"
+  },
+  task: {
+    width: "300px"
+  },
+  status: {
+    width: "50px"
+  },
+  container: {
+    maxHeight: "400px",
+  },
+  box: {
+    display: "flex",
+    padding: "50",
+  },
+  box2: {
+    width: "650px",
+    padding: "100",
+  },
+  box3: {
+    align: "center",
+    width: "100px",
+    padding: "30",
+  },
+});
+
+function createData(no, task, limit, status, iraimoto) {
+  return { no, task, limit, status, iraimoto };
+}
+
+const titles = [
+  createData('No', 'task', 'limit', 'status', 'iraimoto'),
+];
+
+const datas = [
+  createData(1, 'ABC', '20210525', '進行中', 'master'),
+  createData(2, 'aiueo', '20210625', '進行中', 'master'),
+  createData(3, 'あいうえお', '20210527', '未着手', 'master'),
+  createData(4, '１２３４５', '20211025', '未着手', 'master'),
+  createData(5, 'ZZZZ', '20210525', '達成', 'master'),
+  createData(6, '＠＠＠', '20210625', '達成', 'master'),
+  createData(7, 'アイウ', '20210527', '未着手', 'master'),
+  createData(8, '亜伊宇', '20211025', '失敗', 'master'),
+];
+const datas1 = [
+  createData(1,),
+  createData(2,),
+  createData(3,),
+  createData(4,),
+  createData(5,),
+  createData(6,),
+  createData(7,),
+  createData(8,),
+];
+const datas2 = [
+  createData(1),
+  createData(2),
+  createData(3),
+  createData(4),
+  createData(5),
+  createData(6),
+  createData(7),
+  createData(8),
+];
+
+let aaa
+
+export default function TaskList() {
+  const classes = useStyles();
+  const [selected1, setSelected1] = React.useState(String);
+  const [selected2, setSelected2] = React.useState(String);
+  const [datas11, setdatas1] = React.useState(datas1);
+  const [datas22, setdatas2] = React.useState(datas2);
+  const [inputText, setinputText] = React.useState();
+  console.log("aaa",datas11)
+  console.log("aaa",datas22)
+
+  const handleClick1 = (event, name) => {
+    console.log(name)
+    const selectedIndex = selected1.indexOf(name);
+    let newSelected = [];
+    newSelected = newSelected.concat(name);
+    setSelected1(newSelected)
+    console.log(newSelected)
+    console.log(selected1)
+  }
+  const handleClick2 = (event, name) => {
+    console.log(name)
+    const selectedIndex = selected2.indexOf(name);
+    let newSelected = [];
+    newSelected = newSelected.concat(name);
+    setSelected2(newSelected)
+    console.log(newSelected)
+    console.log(selected2)
+  }
+  const infolink = (event, aaa) => {
+    console.log("b-0",datas11)
+    console.log("c-0",datas22)
+    let newDatas1 = datas1
+    let newDatas2 = datas2
+    for (let i = 0; i < newDatas1.length; i++) {
+      if (newDatas1[i].no == selected1) {
+        newDatas1[i].task = selected2
+        break;
+      }
+    }
+    for (let i = 0; i < newDatas2.length; i++) {
+      if (newDatas2[i].no == selected2) {
+        newDatas2.splice(i, 1);
+        console.log("datas2:",datas2)
+        break;
+      }
+    }
+    console.log("b-1",datas11)
+    console.log("c-1",datas22)
+    setdatas1("")
+    setdatas2("")
+    setdatas1(newDatas1);
+    setdatas2(newDatas2)
+    console.log("b-2",datas11)
+    console.log("c-2",datas22)
+    handleClick1(selected1+1)
+    handleClick2(selected2)
+  }
+  const infolinkRe= (event, aaa) => {
+    console.log(aaa);
+    let newDatas1 = datas1
+    let newDatas2 = datas2
+    for (let i = 0; i < newDatas1.length; i++) {
+      if (newDatas1[i].no == selected1) {
+        newDatas2.push(createData(parseInt(newDatas1[i].task)))
+        newDatas1[i].task = 0
+        break;
+      }
+    }
+    newDatas2.sort(function(a, b) {
+      if (a.no > b.no) {
+        return 1;
+      } else {
+        return -1;
+      }
+    })
+    console.log(newDatas2);
+    setdatas1(newDatas1)
+    setdatas2(newDatas2)
+    handleClick1(selected1)
+    handleClick2(selected2)
+
+  }
+  const infolinkAdd= (event, aaa) => {
+  }  
+  const infolinkCl= (event, aaa) => {
+  }  
+  const infolinkBi= (event, aaa) => {
+  }  
+  const printter1 = (name) => {
+    let stPrint
+    for (const elem1 of datas1) {
+      if (elem1.no == name) {
+        for (const elem2 of datas) {
+          if (elem2.no == elem1.task) {
+            stPrint = elem2.task;
+            break;
+          }
+        }
+        break;
+      }
+    }
+    return stPrint;
+  }
+  const printter2 = (name) => {
+    let stPrint
+    for (const elem of datas) {
+      if (elem.no == name) {
+        stPrint = elem.task;
+        break;
+      }
+    }
+    return stPrint;
+  }
+
+  const handleOnChange = (aaa) => {
+    setinputText(aaa);
+    console.log(aaa)
+    console.log(inputText)
+  }
+  return (
+    <div>
+      <div className={classes.box}>
+        <div className={classes.box2}>
+          <TableContainer className={classes.container}>
+            <Table stickyHeader aria-label="sticky table" className={classes.table}>
+              <TableHead>
+                {titles.map((row) => (
+                  <TableRow key={titles.no}>
+                    <TableCell component="th" scope="row">
+                      {row.no}
+                    </TableCell>
+                    <TableCell align="left" className={classes.task} >{row.task}</TableCell>
+                    {/* <TableCell align="left">{row.limit}</TableCell>
+                    <TableCell align="left" className={classes.status}>{row.status}</TableCell>
+                    <TableCell align="left">{row.iraimoto}</TableCell> */}
+                  </TableRow>
+                ))}
+              </TableHead>
+              <TableBody>
+                {datas1.map((row) => {
+                  // const isItemSelected = isSelected(row.name);
+                  return (
+                    <TableRow key={row.no} onClick={(event) => handleClick1(event, row.no)} className={selected1 == row.no ? classes.TableRow : ""}>
+                      <TableCell component="th" scope="row">{row.no}</TableCell>
+                      <TableCell align="left" className={classes.task}>{printter1(row.no)}</TableCell>
+                      {/* <TableCell align="left">{row.limit}</TableCell>
+                      <TableCell align="left" className={classes.status}>{row.status}</TableCell>
+                      <TableCell align="left">{row.iraimoto}</TableCell> */}
+                    </TableRow>
+                  )
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+        <div  className={classes.box3}>
+      <br />
+      <br />
+
+        <button onClick={(event) => infolink(event, "紐づけ")}>
+          紐づけ
+      </button>
+      <br />
+      <br />
+        <button onClick={(event) => infolinkAdd(event, "追加紐づけ")}>
+          追加紐づけ
+      </button>
+      <br />
+      <br />
+        <button onClick={(event) => infolinkRe(event, "戻す")}>
+          戻す
+      </button>
+      <br />
+      <br />
+        <button onClick={(event) => infolinkBi(event, "固定値入力")}>
+          固定値入力
+      </button>
+      <br />
+      <br />
+        <button onClick={(event) => infolinkCl(event, "クリア")}>
+          クリア
+      </button>
+      </div>
+        <div className={classes.box2}>
+          <TableContainer className={classes.container}>
+            <Table stickyHeader aria-label="sticky table" className={classes.table}>
+              <TableHead>
+                {titles.map((row) => (
+                  <TableRow key={titles.no}>
+                    <TableCell component="th" scope="row">
+                      {row.no}
+                    </TableCell>
+                    <TableCell align="left" className={classes.task} >{row.task}</TableCell>
+                    {/* <TableCell align="left">{row.limit}</TableCell>
+                  <TableCell align="left" className={classes.status}>{row.status}</TableCell>
+                  <TableCell align="left">{row.iraimoto}</TableCell> */}
+                  </TableRow>
+                ))}
+              </TableHead>
+              <TableBody>
+                {datas2.map((row) => {
+                  // const isItemSelected = isSelected(row.name);
+                  return (
+                    <TableRow key={row.no} onClick={(event) => handleClick2(event, row.no)} className={selected2 == row.no ? classes.TableRow : ""}>
+                      <TableCell component="th" scope="row">{row.no}</TableCell>
+                      <TableCell align="left" className={classes.task}>{printter2(row.no)}</TableCell>
+                      {/* <TableCell align="left">{row.limit}</TableCell>
+                    <TableCell align="left" className={classes.status}>{row.status}</TableCell>
+                    <TableCell align="left">{row.iraimoto}</TableCell> */}
+                    </TableRow>
+                  )
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+      </div>
+      <br/>
+      <br/>
+      <p>ここに入力<input type="text" value={aaa} /></p>
+      <br/>
+      <br/>
+    </div>
+  );
+
+
+}
